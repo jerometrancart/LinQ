8629d7e846310f2d435594e5d8404c9c6c544a38
jerometrancart <jerome.trancart@gmail.com>
2023-04-24 20:17:34 +0200

    Started LinQ exercises

---
+++ Personne.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace LinQ
+{
+    public enum Sexe 
+    {
+        Homme,
+        Femme
+    }
+    public class Personne
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime? DateNaissance { get; set; }
+        public int TailleCm { get; set; }
+        public Sexe Sexe { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,5 +6,15 @@
 //     System.Console.WriteLine(item);
 // }
 
-int[] tab = new[] { 1, 2, 3, 4, 5};
-System.Console.WriteLine(tab.CompterPair());
+// int[] tab = new[] { 1, 2, 3, 4, 5};
+// System.Console.WriteLine(tab.CompterPair());
+
+List<Personne> personnes = new()
+{
+    new() { Nom = "Dupont", Prenom = "Jean", TailleCm = 175, Sexe = Sexe.Homme },
+    new() { Nom = "Dupont", Prenom = "Jeanne", TailleCm = 156, Sexe = Sexe.Femme, DateNaissance = new(1956, 2, 7)},
+    new() { Nom = "Durand", Prenom = "Marc", TailleCm = 192, Sexe = Sexe.Homme, DateNaissance = new(1959, 10, 5) },
+    new() { Nom = "Thomas", Prenom = "Gérard", TailleCm = 182, Sexe = Sexe.Homme },
+    new() { Nom = "Cerkezoglu", Prenom = "Nurbey", TailleCm = 175, Sexe = Sexe.Homme, DateNaissance = new(1997, 11, 03)},
+    new() { Nom = "Trancart", Prenom = "Jérôme", TailleCm = 175, Sexe = Sexe.Homme, DateNaissance = new(1989, 7, 29)},
+};
